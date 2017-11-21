7b936a27baeef6adc49ec20f798bd8fa6f98821f
Heidycpp <31572400+Heidycpp@users.noreply.github.com>
2017-11-20 21:27:29 -0500

    heidy

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+var friends = [ {
+	"name" : "Bob",
+	"photo" : "https://media.giphy.com/media/ZqlvCTNHpqrio/giphy-facebook_s.jpg",
+	"scores" : [5,5,2,3,1,5,2,1,3,5]
+}, {
+	"name" : "Baby",
+	"photo" : "http://www.nrn.com/sites/nrn.com/files/styles/article_featured_standard/public/bossbaby.png?itok=QsiuDJqt",
+	"scores" : [1,5,1,1,1,1,1,1,5,5]
+}, {
+	"name" : "Homer",
+	"photo" : "http://mrwgifs.com/wp-content/uploads/2015/12/Homer-Chilling-In-a-Pool-With-a-Drink-Cell-Phone-On-The-Simpsons_408x408.jpg",
+	"scores" : [5,3,2,3,2,5,1,3,3,4]
+}];
+
+
+module.exports = friends;
